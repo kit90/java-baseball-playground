72036379179f03bf7cb76fff0ede8c3822699e31
kitcat09 <jk65333@gmail.com>
2023-08-23 21:02:15 +0900

    feat: 입력 숫자 길이 유효성검증
    Balls : 입력 숫자 길이 유효성검증 추가

--- src/main/java/baseball/Balls.java
+++ src/main/java/baseball/Balls.java
@@ -1,16 +1,26 @@
 package baseball;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Balls {
-    public final static int length = 3;
+    public static int length = 3;
     private final List<Ball> balls;
 
+    public static Balls of(List<Integer> integers) {
+        return new Balls(integers);
+    }
+
     public Balls(List<Integer> integers) {
+        validLength(integers);
         this.balls = mapBall(integers);
+    }
+
+    private void validLength(List<Integer> integers) {
+        if(integers.size() != length) {
+            throw new IllegalArgumentException("숫자는 "+length+"자리여야합니다.");
+        }
     }
 
     private static List<Ball> mapBall(List<Integer> integers) {
@@ -19,27 +29,20 @@ public class Balls {
                 .collect(Collectors.toList());
     }
 
-    public BallState play(Ball ball) {
-       /* BallState result = BallState.NOTHING;
-        for (Ball answer : balls) {
-            result = answer.play(ball);
-            if(result.isNotNothing())
-               return result;
+    public GameResult play(Balls userBalls) {
+        GameResult gameResult = new GameResult();
+        for (Ball ball : userBalls.balls) {
+            BallState ballState = this.play(ball);
+            gameResult.plusCountByBallState(ballState);
         }
-        return result;*/
+        return gameResult;
+    }
+
+    public BallState play(Ball ball) {
         return balls.stream()
                 .map(answer -> answer.play(ball))
                 .filter(ballState -> ballState.isNotNothing())
                 .findFirst()
                 .orElse(BallState.NOTHING);
-    }
-
-    public PlayResult play(Balls userBalls) {
-        PlayResult playResult = new PlayResult();
-        for (Ball ball : userBalls.balls) {
-            BallState ballState = this.play(ball);
-            playResult.plusCountByBallState(ballState);
-        }
-        return playResult;
     }
 }
